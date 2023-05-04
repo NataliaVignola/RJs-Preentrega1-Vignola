d954366ef4bcd3f114adfd5dde3558a91dc15c34
Natalia Vignola <natalia.vignola@hotmail.com>
2023-05-04 12:34:21 -0300

    first commit

---
+++ vite-app/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import CartWidget from "./CartWidget/CartWidget"
+
+const NavBar = () => {
+    return
+    <nav>
+        <h3>Locker</h3>
+        <div>
+            <buttom>Muebles</buttom>
+            <buttom>Asientos</buttom>
+            <buttom>Deco</buttom>
+        </div>
+        <CartWidget />
+    </nav>
+}
+
+export default NavBar
